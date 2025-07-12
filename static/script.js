db34cc92d7a2c545c6044f6f82bed682dfe3cadb
Aritra Palit <aritrapalit14@gmail.com>
2025-07-12 09:46:21 +0530

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,1 +1,17 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.getElementById("loginForm");
 
+  form.addEventListener("submit", async function (e) {
+    e.preventDefault();
+
+    const username = document.getElementById("username").value.trim();
+    const password = document.getElementById("password").value.trim();
+
+    const formData = new FormData();
+    formData.append("username", username);
+    formData.append("password", password);
+
+    const response = await fetch("/login", {
+      method: "POST",
+      body: formData
+    });
